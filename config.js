c8df467bafd3a1316c394400586580c278e120ba
Mrkingoff <hebatanakhebat@gmail.com>
2025-02-08 21:02:14 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const global = {
+    owner: ['7507997377'], // GANTI PAKE ID TELE MU YA GANTENG 
+    botToken: "7733562269:AAHoJr2KBVC3N3Nd1mzwU73h7pDVmuL3lFU", // ISI BOT TOKEN LU YAK
+}
+
+module.exports = global;
+// CRACK BY VINNTHEXDC 
+// https://wha+tsapp.com/channel/0029VavOzFy8KMqqMvMnOW1k
